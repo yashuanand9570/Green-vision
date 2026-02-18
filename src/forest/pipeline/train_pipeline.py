534a2ab6015ed7c860402ed95652f8d939bdbb8b
YASH ANAND <143402443+yashuanand9570@users.noreply.github.com>
2026-02-18 23:42:36 +0530

    Create TrainPipeline class for training module

--- src/forest/pipeline/train_pipeline.py
+++ src/forest/pipeline/train_pipeline.py
@@ -1,127 +1,42 @@
-import sys
-from src.forest.components.data_ingestion import DataIngestion
-from src.forest.components.data_validation import DataValidation
-from src.forest.components.data_transformation import DataTransformation
-from src.forest.components.model_trainer import ModelTrainer
-from src.forest.components.model_evaluation import ModelEvaluation
-from src.forest.components.model_pusher import ModelPusher
-from src.forest.exception import ForestException
-from src.forest.logger import logging
-from src.forest.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig,\
-ModelTrainerConfig,ModelEvaluationConfig, ModelPusherConfig
-from src.forest.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact, DataTransformationArtifact,\
-ModelTrainerArtifact,ModelEvaluationArtifact
+import os
+import joblib
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, f1_score
 
 class TrainPipeline:
-    def __init__(self):
-        self.data_ingestion_config = DataIngestionConfig()
-        self.data_validation_config = DataValidationConfig()
-        self.data_transformation_config = DataTransformationConfig()
-        self.model_trainer_config = ModelTrainerConfig()
-        self.model_evaluation_config = ModelEvaluationConfig()
-        self.model_pusher_config = ModelPusherConfig()
+    def __init__(self, data_path:str, model_output_path:str):
+        self.data_path = data_path
+        self.model_output_path = model_output_path
+        self.model = None
 
-    def start_data_ingestion(self) -> DataIngestionArtifact:
-        logging.info("Entered the start_data_ingestion method of TrainPipeline class")
+    def load_data(self):
+        data = pd.read_csv(self.data_path)
+        return data
 
-        try:
-            logging.info("Entered the start_data_ingestion method of TrainPipeline class")
-            logging.info("Getting the data from mongodb")
-            data_ingestion = DataIngestion(data_ingestion_config=self.data_ingestion_config)
-            data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
-            logging.info("Got the train_set and test_set from mongodb")
-            logging.info(
-                "Exited the start_data_ingestion method of TrainPipeline class"
-            )
-            return data_ingestion_artifact
-            
+    def preprocess_data(self, data:pd.DataFrame):
+        # Example preprocessing: filling missing values and splitting features and labels
+        data.fillna(method='ffill', inplace=True)
+        X = data.drop('target', axis=1)
+        y = data['target']
+        return X, y
 
-        except Exception as e:
-            raise ForestException(e, sys) from e
-    
-    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) -> DataValidationArtifact:
-        logging.info("Entered the start_data_validation method of TrainPipeline class")
+    def train_model(self, X:pd.DataFrame, y:pd.Series):
+        X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
+        self.model = RandomForestClassifier()
+        self.model.fit(X_train, y_train)
+        predictions = self.model.predict(X_val)
+        accuracy = accuracy_score(y_val, predictions)
+        f1 = f1_score(y_val, predictions, average='weighted')
+        print(f'Accuracy: {accuracy}, F1 Score: {f1}')
 
-        try:
-            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
-                                             data_validation_config=self.data_validation_config
-                                             )
+    def save_model(self):
+        joblib.dump(self.model, self.model_output_path)
 
-            data_validation_artifact = data_validation.initiate_data_validation()
-
-            logging.info("Performed the data validation operation")
-
-            logging.info(
-                "Exited the start_data_validation method of TrainPipeline class"
-            )
-
-            return data_validation_artifact
-
-        except Exception as e:
-            raise ForestException(e, sys) from e
-    
-    def start_data_transformation(self, data_ingestion_artifact: DataIngestionArtifact) -> DataTransformationArtifact:
-        try:
-            data_transformation = DataTransformation(data_ingestion_artifact=data_ingestion_artifact,
-                                                     data_transformation_config=self.data_transformation_config)
-            data_transformation_artifact = data_transformation.initiate_data_transformation()
-            return data_transformation_artifact
-        except Exception as e:
-            raise ForestException(e, sys)
-
-    def start_model_trainer(self, data_transformation_artifact: DataTransformationArtifact) -> ModelTrainerArtifact:
-        try:
-            model_trainer = ModelTrainer(data_transformation_artifact=data_transformation_artifact,
-                                         model_trainer_config=self.model_trainer_config)
-            model_trainer_artifact = model_trainer.initiate_model_trainer()
-            return model_trainer_artifact
-
-        except Exception as e:
-            raise ForestException(e, sys)
-
-    def start_model_evaluation(self, data_ingestion_artifact: DataIngestionArtifact,
-                               model_trainer_artifact: ModelTrainerArtifact) -> ModelEvaluationArtifact:
-        try:
-            model_evaluation = ModelEvaluation(model_eval_config=self.model_evaluation_config,
-                                               data_ingestion_artifact=data_ingestion_artifact,
-                                               model_trainer_artifact=model_trainer_artifact)
-            model_evaluation_artifact = model_evaluation.initiate_model_evaluation()
-            return model_evaluation_artifact
-        except Exception as e:
-            raise ForestException(e, sys)
-    
-    def start_model_pusher(self, model_trainer_artifact: ModelTrainerArtifact):
-        try:
-            model_pusher = ModelPusher(model_trainer_artifact=model_trainer_artifact,
-                                       model_pusher_config=self.model_pusher_config
-                                       )
-            model_pusher_artifact = model_pusher.initiate_model_pusher()
-            return model_pusher_artifact
-        except Exception as e:
-            raise ForestException(e, sys)
-
-
-    def run_pipeline(self) -> None:
-        logging.info("Entered the run_pipeline method of TrainPipeline class")
-
-        try:
-            data_ingestion_artifact = self.start_data_ingestion()
-            data_validation_artifact= self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
-
-            data_transformation_artifact = self.start_data_transformation(
-                data_ingestion_artifact=data_ingestion_artifact)
-            
-            model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
-
-            model_evaluation_artifact = self.start_model_evaluation(data_ingestion_artifact=data_ingestion_artifact,
-                                                                    model_trainer_artifact=model_trainer_artifact)
-
-            if not model_evaluation_artifact.is_model_accepted:
-                logging.info(f"Model not accepted.")
-                return None
-            model_pusher_artifact = self.start_model_pusher(model_trainer_artifact=model_trainer_artifact)
-
-            logging.info("Exited the run_pipeline method of TrainPipeline class")
-
-        except Exception as e:
-            raise ForestException(e, sys) from e
+    def run(self):
+        data = self.load_data()
+        X, y = self.preprocess_data(data)
+        self.train_model(X, y)
+        self.save_model()
